39ce57067c64f661e865f78c9fb480fe8c80f4c6
mariacarilinass <m.carolina.soares1@gmail.com>
2024-06-25 11:53:17 -0300

    criei classe que mostra a sequência de Fibonacci

---
+++ funcoesrecursivas/src/main/java/com/carolsoares/recursividade/Fibonacci.java
@@ -0,0 +1,37 @@
+package com.carolsoares.recursividade;
+
+/**
+ *
+ * @author soarescarol
+ */
+public class Fibonacci {
+    
+    public String mostrarSequenciaFibonacci(int finalSequencia) {
+        String sequencia = "";
+        for (int i = 1; i < finalSequencia + 1; i++) {
+            sequencia += escreverSequencia(i) + " ";
+        } return sequencia;
+    }
+    
+    private int escreverSequencia(int numSequencia) {
+        if (numSequencia <= 1) {
+            return numSequencia;
+        } return escreverSequencia(numSequencia - 1) + escreverSequencia(numSequencia - 2);
+    } 
+    
+    public String mostrarSequenciaFibonacciIt(int finalSequencia) {
+        int inicioSequencia = 1;
+        int proximoSequencia = 1;
+        String sequenciaFinal = inicioSequencia + " " + proximoSequencia;
+        
+        for (int i = 0; i < finalSequencia-2; i++) {
+            int sequencia = inicioSequencia + proximoSequencia;
+            sequenciaFinal += " " + sequencia;
+            inicioSequencia = proximoSequencia;
+            proximoSequencia = sequencia;     
+        }
+        
+        return sequenciaFinal;
+    }
+    
+}
