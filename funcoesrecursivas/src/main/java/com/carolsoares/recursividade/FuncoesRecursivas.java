e36340f36d212c6c1d814f7d20dd7a0f82b8a252
mariacarilinass <m.carolina.soares1@gmail.com>
2024-07-25 15:15:23 -0300

    adicionei objeto e função da classe Hanoi

--- funcoesrecursivas/src/main/java/com/carolsoares/recursividade/FuncoesRecursivas.java
+++ funcoesrecursivas/src/main/java/com/carolsoares/recursividade/FuncoesRecursivas.java
@@ -22,5 +22,9 @@ public class FuncoesRecursivas {
         String resultadoFibonacci= numFibonacci.mostrarSequenciaFibonacci(10);
         System.out.println(resultadoFibonacci);
         
+        // mostrar movimentos da Torre de Hannoi
+        Hannoi hannoi = new Hannoi();
+        int movimentos = hannoi.mostrarMovimentosTorreHannoi(3, "A", "B", "C");
+        System.out.println("Total de movimentos: " + movimentos);
     }
 }
